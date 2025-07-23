d2d352b5378d5482e78579aa1a727252fed2e29b
BiagioAltruda <biagio.altruda02@gmail.com>
2025-07-23 09:20:41 +0200

    finishing touches

--- src/main/webapp/script/scriptFooter.js
+++ src/main/webapp/script/scriptFooter.js
@@ -1,5 +1,5 @@
 // Initialization for ES Users
 //serve per pulire la mail dove che è ststa inviata alla newsletter
-import { Input, initMDB } from "mdb-ui-kit";
+//import { Input, initMDB } from "mdb-ui-kit";
 
-  initMDB({ Input });
+//initMDB({ Input });
